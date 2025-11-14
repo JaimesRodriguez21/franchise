ec2408325807e2b7b3ada235d6257a1143f85f8b
josegabriel-jr <jaimesrodriguezjg@gmail.com>
2025-11-13 22:37:40 -0500

    feat: add models

---
+++ domain/model/src/main/java/co/com/franchise/model/product/Product.java
@@ -0,0 +1,19 @@
+package co.com.franchise.model.product;
+import lombok.Builder;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class Product {
+
+    private String id;
+    private String name;
+    private String storeId;
+    private int stock;
+}
